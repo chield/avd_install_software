0b7a971f56955a1b9781d435c550e0dd8fd2efc9
Michiel Dekker <33653504+chield@users.noreply.github.com>
2022-10-19 09:58:12 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,12 +5,6 @@
 # Foxit Reader Enterprise Packaging (requires registration)
 # https://kb.foxitsoftware.com/hc/en-us/articles/360040658811-Where-to-download-Foxit-Reader-with-Enterprise-Packaging-MSI-
 # 
-# Notepad++
-# https://notepad-plus-plus.org/downloads/v7.8.8/
-# See comments on creating a custom setting to disable auto update message
-# https://community.notepad-plus-plus.org/post/38160
-
-
 
 #region Set logging 
 $logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
